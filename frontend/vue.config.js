c16b02f34828e96b71981e2a3fdc7780e035d3f2
Jason Doss <jason@dossweb.com>
2018-10-01 20:34:19 -0400

    Initial commit of Vue Now UI Dashboard Pro with Laravel 5.7 backend.
    https://github.com/yyx990803/laravel-vue-cli-3

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+module.exports = {
+  lintOnSave: true,
+  // output built static files to Laravel's public dir.
+  // note the "build" script in package.json needs to be modified as well.
+  outputDir: '../public',
+
+  // modify the location of the generated HTML file.
+  // make sure to do this only in production.
+  indexPath: process.env.NODE_ENV === 'production'
+      ? '../resources/views/index.blade.php'
+      : 'index.html'
+};
